b206c8eeca2fb39abb88e018e0d7b0cc5f1f48b8
hui0802 <130963614+hui0802@users.noreply.github.com>
2023-06-05 14:58:48 +0800

    Add files via upload

---
+++ pen-export-rNQBwVb/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hui0802/pen/rNQBwVb](https://codepen.io/hui0802/pen/rNQBwVb).
+
---
+++ pen-export-rNQBwVb/dist/script.js
@@ -0,0 +1,22 @@
+document.write("你好啊!!!")
+.video-back{ 
+
+position:fixed;
+
+top:0;   
+
+left:0;
+
+min-width:100%;
+
+min-height:100%;
+
+z-index:-1;  
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count++;
+      counterElement.textContent = count;
+    }
+}
